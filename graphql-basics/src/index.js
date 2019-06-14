3322749334fb5cb0220eede958534620134d3297
Aaron <aaronrbeck@gmail.com>
2019-06-14 12:50:59 -0600

    15 - operation arguments,

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -4,8 +4,10 @@ import { GraphQLServer } from 'graphql-yoga'
 //14 Type definitaions (shema)
 const typeDefs =`
     type Query {
+        greeting(name: String, position: String): String!
         me: User!
         post: Post!
+        add(a: Float!, b: Float!): Float!
     }
 
     type User {
@@ -20,11 +22,25 @@ const typeDefs =`
         body: String!
         published: Boolean!
     }
+    
 `
 
 //12 challenge Resolvers
 const resolvers = {
+
     Query: {
+        add(parent, args, ctx, info){
+            return args.a + args.b
+        },
+        greeting(parent, args, ctx, info){
+            if (args.name && args.position){
+                return `Hello, ${args.name}! You are my favorite ${args.position}.`
+            } else {
+                return "Hello"
+            }
+            
+            
+        },
         me(){
             return{
                 id: '123098',
