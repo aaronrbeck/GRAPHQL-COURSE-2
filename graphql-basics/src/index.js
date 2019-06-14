0d4c2a24687c9991c8439a66f2600d8700c42c0f
Aaron <aaronrbeck@gmail.com>
2019-06-14 12:03:14 -0600

    11 - set up index.js with graphql-yoga, defined typedef and resolver objects

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,39 @@
-console.log('hello gql')
+//11 npm install graphql-yoga then import
+import { GraphQLServer } from 'graphql-yoga'
+
+//11 Type definitaions (shema)
+const typeDefs =`
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
+
+//11 Resolvers
+const resolvers = {
+    Query: {
+        hello(){
+            return "this is first query"
+        },
+        name(){
+            return "andrew mead"
+        },
+        bio(){
+            return "My name is aaron.  I'm from Idaho"
+        },
+        location(){
+            return "idaho"
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up')
+})
