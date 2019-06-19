37abde8bc0e14803ebaeb142fe73cc4344af9d3c
Aaron <aaronrbeck@gmail.com>
2019-06-19 17:07:00 -0600

    burned out - queries in prisma.js all have unhandled promises for some reason

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -19,73 +19,88 @@ const prisma = new Prisma({
 // prisma.query  prisma.mutation prisma.subscription prisam.exists
 
     //all prisma methods take 2 arguments: operation and selection set
-// prisma.query.users(null, '{ id name posts { id title } }')
-    //the above returns a promise so
-// .then((data) => {
-        // 51 use the JSON.stringify with 3 defined parameters 
-        // to make console output more Readable similar 
-        // formatt to playground
-// console.log(JSON.stringify(data, undefined, 2))
-// }).catch((error)=>{
-// console.error("there is an error in prisma.query.users")
-// })
 
 
-    // 51 challenge:
-    // use comments query to fetch all comments, grab id and text
-    // grab comment author id and name
-    // view in terminal
-// prisma.query.comments(null, '{ id text author {id name}}').then((data)=>{
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
+
+//53
+//1. create a new post
+//2.  fetch all info about user(author)
+
+const createPostForUser = async (authorId, data) => {
+    const post = await prisma.mutation.createPost({
+        data:{
+            ...data,
+            author:{
+                connect:{
+                    id: authorId
+                }
+
+            }
+        }
+    }, '{ id }')
+    const user = await prisma.query.user({
+        where:{
+            id: authorId
+        }
+    }, '{ id name email posts { id title published }}')
+    return user 
+}
+    // createPostForUser('cjx3ai49e00fv0791tf4bn3fh', {
+    //     title: "Greate books to read",
+    //     body: "The War of Art",
+    //     published: true
+    // }).then((user) => {
+    //     console.log(JSON.stringify(user, udefined, 2))
+    // })
 
 
-// 52 node origination mutation
-// prisma.mutation.createPost({
-//     data: {
-//         title: "GQL 101",
-//     body: "",
-//     published: false,
-//     author:{
-//         connect: {
-//             id: "cjx3ai49e00fv0791tf4bn3fh"
-//         }
+
+    //53 challenge - use asnync/await with prisma bindings
+    //-create updatePostForUser that accepts the post id and data to update
+    //update the post (get author id back)
+    //fetch the user associated with the updated post and returne the user data
+    //grab some fields grabbed for createPostForUser
+    //call the function with the ide and data and use a then methods to get the user information
+    //print the user infor ot the console and test your work
+
+
+ const updatePostForUser = async (postId, data) => {
+ const post = await prisma.mutation.updatePost({
+     where: {
+         id: postId
+     },
+     data
+
+     }, //this second argument is the selection set:
+     '{ author { id } }')
+     const user = await prisma.query.user({
+         where: {
+             id: post.author.id
+         }
+     },//selection set for query:
+     '{ id name email post { it title published }}')
+     return user       
+}
+
+
+updatePostForUser('cjx39aci400de07911nd79h81', { published: false }).then((user) => {
+        console.log(JSON.stringify(user, undefined, 2))
+    })
+
+    
+
+// prisma.mutation.updatePost({
+//     where:{
+//         id: "cjx3rr79h00tp0791witxl5mk"
+//     },
+
+//     data:{
+//         body:"this is how to get started with gql",
+//             published: true
 //     }
-// }
 // }, '{ id title body published }').then((data) => {
 //     console.log(data)
-            //***** */move query call into this as a chained async call:
-//     return prisma.query.users(null, '{ id name posts { id title }}')
+//     return prisma.query.posts(null, '{ id title body published }')
 // }).then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
+//     console.log(data)
 // })
-            // ********since I've been getting UnhandledPromiseRejectionWarning: 
-            // ********Unhandled promise rejection., I will:
-// .catch((error)=>{
-// console.error(" there is an error in prisma.query.users")
-// })
-            // *****the above seems to be writing and returning but
-            // *****I'm still getting an unhandle promise rejection error
-            // *****for some reason and my error message is not running
-            // *****for some reason
-
-
-            // ******52 challange
-            // ******updated newly created post changing body, mark as published
-            // ******fetch all posts (id, title, body, published) print to console
-            // ******view the list of posts and confirm updates
-prisma.mutation.updatePost({
-    where:{
-        id: "cjx3rr79h00tp0791witxl5mk"
-    },
-
-    data:{
-        body:"this is how to get started with gql",
-            published: true
-    }
-}, '{ id title body published }').then((data) => {
-    console.log(data)
-    return prisma.query.posts(null, '{ id title body published }')
-}).then((data) => {
-    console.log(data)
-})
