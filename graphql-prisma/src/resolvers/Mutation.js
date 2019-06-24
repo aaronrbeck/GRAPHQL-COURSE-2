bf576cdb5e4c6b6ed5a230cd9e2c3545de3bbb4e
Aaron <aaronrbeck@gmail.com>
2019-06-24 15:17:01 -0600

    63 - refactored mutation.js through create post 63 - refactored mutation.js through create post to use prisma

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,89 +1,55 @@
 
 import uuidv4 from 'uuid/v4'
+import prisma from '../prisma'
+import { assign } from 'apollo-utilities';
 
 
 const Mutation = {
-    createUser(parent, args, { db }, info){
-        const emailTaken = db.users.some((user) => user.email === args.data.email)
-            if (emailTaken){
+    async createUser(parent, args, { primsa }, info){
+const emailTaken = await prisma.exists.User({ email: args.data.email }, info)
+    if (emailTaken){
     throw new Error('Email taken')
 }
-const user = {
-    id: uuidv4(),
-    ...args.data
+//create the user with prisma:
+return prisma.mutation.createUser({data: args.data}, info)
+},
+
+
+
+async deleteUser(parent, args, { prisma }, info){
+const userExists = await prisma.exists.User({ id: args.id})
+if (!userExists){
+    throw new Error('User not found')
 }
-db.users.push(user)
-return user
-
+return prisma.mutation.deleteUser({
+    where: {
+        id: args.id
+    }
+}, info)
+},
+async updateUser(parent, args, { prisma }, info){
+    return prisma.mutation.updateUser ({
+        where:{
+            id: args.id
         },
-deleteUser(parent, args, { db }, info){
-    const userIndex = users.findIndex((user) => user.id === args.id)
-    if (userIndex === -1) {
-        throw new Error('User not found')
-    }
-    const deletedUsers = db.users.splice(userIndex, 1)
-
-    db.posts = db.posts.filter((post) => {
-        const match = post.author === args.id
-        if (match) {
-            db.comments = db.comments.filter((comment) => comment.post !== post.id)
-        }
-        return !match
-    }
-    )
-    db.comments = db.comments.filter((comment) => comment.author !== args.id)
-    return deletedUsers[0]
-
+        data: args.data
+    }, info)
 },
-updateUser(parent, args, { db }, info){
-    const { id, data } = args
-    const user = db.users.find((user)=> user.id === id)
-    if (!user){
-        throw new Error ('User not found')
-
-    }
-    if (typeof data.email === 'string'){
-        const emailTaken = db.users.some((user) => user.email === data.email)
-        if (emailTaken){
-            throw new Error ('Email taken')
-
-        }
-        user.email = data.email
-    }
-    if (typeof data.name === 'string'){
-        user.name = data.name
-    }
-    if (typeof data.age !== 'undefined'){
-        user.age = data.age
-    }
-    return user
-},
-createPost(parent, args, { db, pubsub }, info){
-    const userExists = db.users.some((user) => user.id === args.data.author)
-    if (!userExists) {
-        throw new Error('User not found')
-
-    }
-    const post = {
-        id: uuidv4(),
-        ...args.data
-    }
-    db.posts.push(post)
-    //lesson 37 challeng added the pubsub.publish line.  I'm not clear on the two parameters defined below
-    if (args.data.published){
-        //lesson 38 modified to accomodate payload formatting from:
-        // pubsub.publish('post', { post })
-        //to:
-        pubsub.publish('post',{
-            post:{
-                mutation: 'CREATED',
-                data: post
+async createPost(parent, args, { prisma }, info){
+    return prisma.mutation.createPost ({
+        
+        data: {
+            title: args.data.title,
+            body: args.data.body,
+            published: args.data.published,
+            author:{
+                connect:{
+                    id: args.data.author
+                }
             }
-        })
+        }
+    }, info)
 
-
-    }
-    return post
 
 },
 deletePost(parent, args, { db, pubsub }, info){
