465d6d8602fe9995131eb27a756bbb144bb74442
Aaron <aaronrbeck@gmail.com>
2019-06-25 15:01:03 -0600

    69 - use JSON web token to provide a password token

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,28 +1,46 @@
 import  bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import prisma from '../prisma'
 import { assign } from 'apollo-utilities';
 
 //68 take in pw, validate pw length, hash pw, generate auth token
+//69 use JSON web token to provide a password token
+    //install jsonwebtoken
+    //learning:
+    // jwt.sign({payload}, secret )
+    const token = jwt.sign({ id: 46 }, 'mysecret')
+    //payload is not meant to be encrypted - it is public
+    console.log(token)
+    const decoded = jwt.decode(token) 
+    console.log(decoded)
+    //we only want to trust what comes back from verified web tokens
+    //so:
+    const decoded2 = jwt.verify(token, 'mysecret')
+    console.log(decoded2)
+    //copy webtoken from console, take to jwt.io
+    //token sections: header, payload, signiture, seperated by periods, signature is a hash
+
 const Mutation = {
-    async createUser(parent, args, { primsa }, info){
+    async createUser(parent, args, { prisma }, info){
         if (args.data.password.length < 8){
             throw new Error ('Password must be 8 characters or longer.')
         }
         //take in plain text pw, return a hashed version
         //take in the user pw from args.data and specify a salt(a number of random numbers to include in hashing)
         const password = await bcrypt.hash(args.data.password, 10)
-        //     const emailTaken = await prisma.exists.User({ email: args.data.email }, info)
-//     if (emailTaken){
-//     throw new Error('Email taken')
-// }
-return prisma.mutation.createUser({
+        //69 jws
+        const user = await prisma.mutation.createUser({
     //68 modified the data parameter to an object in 
     //order to include password
     data: {
         ...args.data,
         password
     }
-}, info)
+})
+    return {
+        user,
+        token: jwt.sign({ userId: user.id }, 'thisisasecret')
+    }
 },
 
 
