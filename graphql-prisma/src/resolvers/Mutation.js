853eb35c9511b80690c24dfc7ca70195ecf874c7
Aaron <aaronrbeck@gmail.com>
2019-06-25 10:49:26 -0600

    input CreateUserInput password error question

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,17 +1,28 @@
-
-import uuidv4 from 'uuid/v4'
+import  bcrypt from 'bcryptjs'
 import prisma from '../prisma'
 import { assign } from 'apollo-utilities';
 
-
+//68 take in pw, validate pw length, hash pw, generate auth token
 const Mutation = {
     async createUser(parent, args, { primsa }, info){
-const emailTaken = await prisma.exists.User({ email: args.data.email }, info)
-    if (emailTaken){
-    throw new Error('Email taken')
-}
-//create the user with prisma:
-return prisma.mutation.createUser({data: args.data}, info)
+        if (args.data.password.length < 8){
+            throw new Error ('Password must be 8 characters or longer.')
+        }
+        //take in plain text pw, return a hashed version
+        //take in the user pw from args.data and specify a salt(a number of random numbers to include in hashing)
+        const password = await bcrypt.hash(args.data.password, 10)
+        //     const emailTaken = await prisma.exists.User({ email: args.data.email }, info)
+//     if (emailTaken){
+//     throw new Error('Email taken')
+// }
+return prisma.mutation.createUser({
+    //68 modified the data parameter to an object in 
+    //order to include password
+    data: {
+        ...args.data,
+        password
+    }
+}, info)
 },
 
 
